6114b58c0b18c1b97a4474eab262c56d5a08afbb
Anonyo <Anonyo@LAPTOP-LPCVOOVC>
2020-10-16 09:35:21 +0530

    added transaction thru master

---
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -0,0 +1,29 @@
+package com.lti.model;
+
+import java.time.LocalDate;
+
+public class Transaction {
+	int TransactionId;
+	LocalDate transactionDate;
+	double amount;
+	public int getTransactionId() {
+		return TransactionId;
+	}
+	public void setTransactionId(int transactionId) {
+		TransactionId = transactionId;
+	}
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(LocalDate transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+}
